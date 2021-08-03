d2ec7fcb8d0bdff6f158dbcd310f4008fa2677dd
Radhika <radhika.shankar1220@gmail.com>
2021-08-03 10:32:40 +0530

    [Radhika] Creating a connection and retrieve the data

---
+++ PayrollServicesADO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PayrollServicesADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a object for employeerepository
+            EmployeeRepository repository = new EmployeeRepository();
+            //Calling the method
+            repository.GetAllEmployee();
+        }
+    }
+}
