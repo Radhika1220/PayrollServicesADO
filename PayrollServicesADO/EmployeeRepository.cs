fc24102cbd1dfa7eeb2644bb51653eebf56bb169
Radhika <radhika.shankar1220@gmail.com>
2021-08-03 12:39:40 +0530

    [Radhika] update the salar column for particular column

--- PayrollServicesADO/EmployeeRepository.cs
+++ PayrollServicesADO/EmployeeRepository.cs
@@ -7,20 +7,26 @@ namespace PayrollServicesADO
 {
     public class EmployeeRepository
     {
+        /// <summary>
+        /// UC1-Sql server connection
+        /// </summary>
         //Connecting to Database
         public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Payroll_Services;Integrated Security=True";
         //passing the string to sqlconnection to make connection 
         SqlConnection sqlconnection = new SqlConnection(connectionString);
-        //GetAllEmployee method
+
+        /// <summary>
+        /// UC2-Retrieve the data(select query)
+        /// </summary>
         public void GetAllEmployee()
-        {
+        {     
             try
             {
                 //Creating object for employeemodel and access the fields
                 EmployeeModel employeeModel = new EmployeeModel();
                 //Retrieve query
                 string query = @"select * from employee_payroll";
-                
+
                 SqlCommand sqlCommand = new SqlCommand(query, sqlconnection);
                 //Open the connection
                 this.sqlconnection.Open();
@@ -30,7 +36,7 @@ namespace PayrollServicesADO
                 {
                     while (reader.Read())
                     {
-                        
+
                         employeeModel.empId = Convert.ToInt32(reader["empId"]);
                         employeeModel.name = reader["name"].ToString();
                         employeeModel.BasicPay = Convert.ToDouble(reader["BasicPay"]);
@@ -44,7 +50,7 @@ namespace PayrollServicesADO
                         employeeModel.TaxablePay = Convert.ToDouble(reader["TaxablePay"]);
                         employeeModel.IncomeTax = Convert.ToDouble(reader["IncomeTax"]);
                         employeeModel.NetPay = Convert.ToDouble(reader["NetPay"]);
-                        Console.WriteLine("{0} {1} {2}  {3} {4} {5}  {6}  {7} {8} {9} {10} {11} {12}", employeeModel.empId, employeeModel.name, employeeModel.BasicPay, employeeModel.startDate, employeeModel.emailId, employeeModel.Gender, employeeModel.Department, employeeModel.PhoneNumber,employeeModel.Address,employeeModel.Deductions,employeeModel.TaxablePay,employeeModel.IncomeTax,employeeModel.NetPay);
+                        Console.WriteLine("{0} {1} {2}  {3} {4} {5}  {6}  {7} {8} {9} {10} {11} {12}", employeeModel.empId, employeeModel.name, employeeModel.BasicPay, employeeModel.startDate, employeeModel.emailId, employeeModel.Gender, employeeModel.Department, employeeModel.PhoneNumber, employeeModel.Address, employeeModel.Deductions, employeeModel.TaxablePay, employeeModel.IncomeTax, employeeModel.NetPay);
                         Console.WriteLine("\n");
                     }
 
@@ -63,6 +69,39 @@ namespace PayrollServicesADO
             {
                 this.sqlconnection.Close();
             }
+        }
+        /// <summary>
+        /// UC3-Update the salary data using query for particular data
+        /// </summary>
+        /// <param name="model"></param>
+        public void UpdateSalary(EmployeeModel model)
+        {
+            try
+            { 
+                    sqlconnection.Open();
+                   string query = @"update employee_payroll set BasicPay=3000000 where name='Radhika'";
+                    SqlCommand command = new SqlCommand(query, sqlconnection);
+                   
+                    int result = command.ExecuteNonQuery();
+                    if (result != 0)
+                    {
+                        Console.WriteLine("Salary Updated Successfully ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unsuccessfull");
+                    }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sqlconnection.Close();
+
+            }
+         
         }
     }
 }
--- PayrollServicesADO/Program.cs
+++ PayrollServicesADO/Program.cs
@@ -8,8 +8,20 @@ namespace PayrollServicesADO
         {
             //Creating a object for employeerepository
             EmployeeRepository repository = new EmployeeRepository();
+            EmployeeModel model = new EmployeeModel();
+            Console.WriteLine("Enter 1-To Retrieve all Data from Sql server\nEnter 2-To Update Salary to 3000000\n");
             //Calling the method
-            repository.GetAllEmployee();
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    repository.GetAllEmployee();
+                    break;
+                case 2:
+                    repository.UpdateSalary(model);
+                    repository.GetAllEmployee();
+                    break;
+            }
         }
     }
 }
